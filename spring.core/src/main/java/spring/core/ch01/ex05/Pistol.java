539a18dfe523ebf67e3b56180c1c3e8e7938bfb6
'yunghyunchoi' <'asl321@naver.com'>
2023-03-06 14:28:39 +0900

    a

--- spring.core/src/main/java/spring/core/ch01/ex05/Pistol.java
+++ spring.core/src/main/java/spring/core/ch01/ex05/Pistol.java
@@ -3,7 +3,7 @@ package spring.core.ch01.ex05;
 import org.springframework.stereotype.Component;
 
 @Component
-public class pistol implements Gun {
+public class Pistol implements Gun {
 	@Override
 	public void fire() {
 		System.out.println("권총 탕탕.");
